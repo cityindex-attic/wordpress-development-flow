b7973adb2c6590d3a35693074f6e9763d00532b4
David Laing <david@davidlaing.com>
2013-03-17 00:06:10 +0000

    Add stackato deploy

---
+++ http-server.js
@@ -0,0 +1,10 @@
+var connect = require('connect');
+
+var port = process.env.PORT || 3001;
+var host = process.env.VCAP_APP_HOST || "127.0.0.1";
+
+var app = connect()
+  .use(connect.logger())
+  .use(connect.static(__dirname + '/', { maxAge: 0 }))
+  .listen(port, host);
+console.log("Listening on http://%s:%d", host, port);
---
+++ scripts/deploy.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+if [ -z "$STACKATO_PASSWORD" ] then
+	echo "STACKATO_PASSWORD must be defined"
+fi
+
+parse_git_branch() {
+   git branch 2> /dev/null | sed -e '/^[^*]/d' -e 's/* \(.*\)/(\1)/'
+}
+
+DEPLOY_NAME="$(parse_git_branch)-rdb"
+
+stackato target https://api.stackato.cil.stack.me
+stackato login david.laing@labs.cityindex.com --pass $STACKATO_PASSWORD
+stackato push $DEPLOY_NAME --no-prompt
+[ $? -ne 0 ]; then
+    stackato update $DEPLOY_NAME --no-prompt
+fi

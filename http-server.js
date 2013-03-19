1b1e35a64a7ff49993535c89f9108e01e841cc18
David Laing <david@davidlaing.com>
2013-03-19 13:17:00 +0000

    Point http-server at dist/

--- http-server.js
+++ http-server.js
@@ -5,6 +5,6 @@ var host = process.env.VCAP_APP_HOST || "127.0.0.1";
 
 var app = connect()
   .use(connect.logger())
-  .use(connect.static(__dirname + '/', { maxAge: 0 }))
+  .use(connect.static(__dirname + '/dist', { maxAge: 0 }))
   .listen(port, host);
 console.log("Listening on http://%s:%d", host, port);

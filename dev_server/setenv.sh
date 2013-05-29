97dd7d04891d57caf5847ee3780d398dbf79b20c
David Laing <david@davidlaing.com>
2013-05-29 17:01:10 +0100

    Rename /src to /app and move DEV server specific scripts into dev_server/

--- dev_server/setenv.sh
+++ dev_server/setenv.sh
@@ -1,6 +1,6 @@
 #Simulate the Stackato container environment
 export DATABASE_URL=mysql://root:secret_password@127.0.0.1:3306/wordpress
 export DATABASE_ALWAYS_REFRESH="true"
-export DATABASE_SQL_DUMP="/vagrant/src/db/wordpress.master.sql"
-export STACKATO_APP_ROOT=/home/vagrant
-export STACKATO_DOCUMENT_ROOT=/home/vagrant/dist
+export STACKATO_APP_ROOT=/app
+export STACKATO_DOCUMENT_ROOT=/app/app
+export DATABASE_SQL_DUMP="${STACKATO_DOCUMENT_ROOT}/db/wordpress.master.sql"
---
+++ dev_server/sync_app_on_host_2_app.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/env ruby
+require 'listen'
+require 'fileutils'
+
+puts "rsyncing /tmp/app_on_host/ -> /app/app"
+`rsync -a --exclude='.git*' --exclude='.vagrant' --exclude='.DS_Store' /tmp/app_on_host/ /app/app/`
+
+puts "Watching /tmp/app_on_host/ for new changes ..."
+Listen.to("/tmp/app_on_host/", :force_polling => true, :latency => 0.5 ) do |modified, added, removed|
+	
+	puts "Detected new files #{added.inspect}" unless added.empty?
+	puts "Detected modifications to #{modified.inspect}" unless modified.empty?
+	(modified << added).flatten.each do |src_file|
+    targetLocation = src_file.sub('/tmp/app_on_host/', '/app/app/')  
+		puts "\tcopying #{src_file} to #{targetLocation}"
+
+		FileUtils.mkdir_p(File.dirname(targetLocation));  
+		FileUtils.cp(src_file, targetLocation) unless File.directory?(src_file)
+  end
+
+  unless removed.empty? 
+		 puts "Detected deleted files #{removed.inspect}"
+		 removed.each do |removed_file|
+      targetLocation = removed_file.sub('/tmp/app_on_host/', '/app/app/')  
+      puts "\tdeleting #{targetLocation}"
+      FileUtils.rm(targetLocation)  
+    end
+	end
+
+end
